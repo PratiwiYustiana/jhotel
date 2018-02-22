fb8384f991333dab6ac0377e41d3b1506ab69cd6
PratiwiYustiana <pratiwi.yustiana@ui.ac.id>
2018-02-22 18:19:18 +0700

    modul 1

---
+++ jhotel/Customer.java
@@ -0,0 +1,48 @@
+
+/**
+ * Write a description of class Customer here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Customer
+{
+    /**
+     * Constructor for objects of class Customer
+     */
+    protected Customer()
+    {
+        // initialise instance variables
+        int id = 0;
+        String nama = null;
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void Customer(int id, String nama)
+    {
+        // put your code here
+        return;
+    }
+    public int getID()
+    {
+        return 0;
+    }
+    public String getNama()
+    {
+        return null;
+    }
+    public void setID(int id)
+    {
+        return;
+    }
+    public void setNama(String nama)
+    {
+        return;
+    }
+}
---
+++ jhotel/DatabaseCustomer.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class DatabaseCustomer here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DatabaseCustomer
+{
+    /**
+     * Constructor for objects of class DatabaseCustomer
+     */
+    private DatabaseCustomer()
+    {
+        // initialise instance variables
+        String[] lis_customer = null;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public boolean addCustomer(Customer baru)
+    {
+        // put your code here
+        return false;
+    }
+    public boolean removeCustomer(int id)
+    {
+        return false;
+    }
+    public String[] getCustomerDatabase()
+    {
+        return null;
+    }
+}
